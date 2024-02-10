3202014c25c5047430e3906ed57a28bc11af10b0
Adam Waggoner <adam.swaggoner@gmail.com>
2024-02-10 16:33:36 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () =>
+{
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h2>Project PRISM</h2>
+        </div>
       </header>
+      <p>booooo</p>
+      <footer className="App-footer">
+        <h6>Created by <a href="https://www.absurdwalls.com">ABSURD WALLS</a> | Inspired by <a href="https://adarkroom.doublespeakgames.com/">A Dark Room</a></h6>
+      </footer>
     </div>
   );
 }
